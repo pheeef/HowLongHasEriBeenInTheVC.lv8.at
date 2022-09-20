58bed3716e7c9b873e71b66d7f852be96db253e2
Philipp Lackinger <phil@philsoft.at>
2022-09-20 23:59:54 +0200

    ✨ 1.0.2 Update Date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,43 +4,48 @@
 	import updateLocale from 'dayjs/plugin/updateLocale';
 	import duration from 'dayjs/plugin/duration';
 
+	import { goto } from '$app/navigation';
+
 	dayjs.extend(relativeTime);
 	dayjs.extend(updateLocale);
 	dayjs.extend(duration);
 
-	let now = dayjs();
+	let now = dayjs('2022-09-20T20:09:00.000Z');
 	const startTime = dayjs('2022-09-13T17:00:00.000Z');
+	let newdate: duration.Duration;
 
 	let nowUnix = now.unix();
 	const startTimeUnix = startTime.unix();
 
 	let difference = nowUnix - startTimeUnix;
 
-	let newdate;
+	newdate = dayjs.duration(difference, 's')
 
-	function calcDiff() {
-		now = dayjs();
-		nowUnix = now.unix();
 
-		//console.log("Unix Now", nowUnix)
-		//console.log("Unix Then", startTimeUnix)
+	// function calcDiff() {
+	// 	now = dayjs();
+	// 	nowUnix = now.unix();
 
-		difference = nowUnix - startTimeUnix;
-		//console.log("Difference", difference)
+	// 	//console.log("Unix Now", nowUnix)
+	// 	//console.log("Unix Then", startTimeUnix)
 
-		newdate = dayjs.duration(difference, 's');
-		//console.log(newdate)
-	}
+	// 	difference = nowUnix - startTimeUnix;
+	// 	//console.log("Difference", difference)
 
-	$: calcDiff();
+	// 	newdate = dayjs.duration(difference, 's');
+	// 	//console.log(newdate)
+	// }
 
-	setInterval(() => {
-		calcDiff();
-	}, 1000);
+	// $: calcDiff();
+
+	// setInterval(() => {
+	// 	calcDiff();
+	// }, 100);
+
+
 </script>
-
 <main class="absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 flex flex-col justify-items-center justify-center gap-10">
-    <h1 class="text-7xl text-gray-700 dark:text-white">Eri has been in the VC for:</h1>
+    <h1 class="text-7xl text-gray-700 dark:text-white">Eri was in the VC for:</h1>
 	<div class="grid grid-flow-col gap-5 text-center auto-cols-max justify-center">
 		<div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
 			<span class="countdown font-mono text-5xl">
@@ -67,6 +72,15 @@
 			sec
 		</div>
 	</div>
-    <h2 class="text-center">v1.0.1 ~ Made with ❤️ by Pheeef</h2>
+    <h2 class="text-center">v1.0.2 ~ Made with ❤️ by <span class="link" on:click={async () => await goto("/rickroll")}>Pheeef</span></h2>
 </main>
 
+<style lang="postcss">
+	.link{
+		@apply text-purple-400;
+	}
+
+	.link:hover{
+		@apply text-purple-600;
+	}
+</style>
---
+++ src/routes/rickroll/+page.svelte
@@ -0,0 +1,9 @@
+<main>
+    <video style="width: 100vw; height: 100vh; left: 0px; top: 0px;" tabindex="-1" class="video-stream html5-main-video" src="video.mp4" loop=true autoplay />
+</main>
+
+<style>
+    video::-webkit-media-controls {
+		display: none;
+	}
+</style>
